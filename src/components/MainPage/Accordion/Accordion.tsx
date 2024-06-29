ef9b2f2f838390da4e773aa34a3bf47c682ef6a9
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2024-06-29 18:10:15 +0300

    Add favicon, add accordion Item<AccordionComponent

--- src/components/MainPage/Accordion/Accordion.tsx
+++ src/components/MainPage/Accordion/Accordion.tsx
@@ -1,47 +1,22 @@
 import React, { useState } from 'react';
-import AccordionItem from './AccordionItem';
-
-import img1 from "../../../assets/images/mainImages/desktop1920/main-facilities1-desktop-1920.webp"
-import img2 from "../../../assets/images/mainImages/desktop1920/main-facilities2-desktop-1920.webp"
-import img3 from "../../../assets/images/mainImages/desktop1920/main-facilities3-desktop-1920.webp"
-
-const items = [
-  {
-    id: 1,
-    image: img1,
-    text: 'Ice restaurant',
-    fullText: 'The hotel’s exclusive infrastructure is complemented by the unique atmosphere of the Safe Restaurant. Author’s menu, extensive wine card, and live music will set you for the correct mood.',
-    isOpen: true,
-  },
-  {
-    id: 2,
-    image: img2,
-    text: 'Conference hall',
-    fullText: 'The hotel’s exclusive infrastructure is complemented by the unique atmosphere of the Safe Restaurant. Author’s menu, extensive wine card, and live music will set you for the correct mood.',
-    isOpen: false,
-  },
-  {
-    id: 3,
-    image: img3,
-    text: 'Wine bar “reserve”',
-    fullText: 'The hotel’s exclusive infrastructure is complemented by the unique atmosphere of the Safe Restaurant. Author’s menu, extensive wine card, and live music will set you for the correct mood.',
-    isOpen: false,
-  }
-];
+import AccordionItem from './AccordionItem/AccordionItem';
+import { items } from '../../../helpers/accordionItems';
 
 const Accordion: React.FC = () => {
     const [openItemId, setOpenItemId] = useState<number | null>(1);
+    const [isArrowOpen, setIsArrowOpen] = useState<boolean>(false);
 
     const handleToggle = (id: number) => {
         setOpenItemId(openItemId === id ? null : id);
+        setIsArrowOpen(!isArrowOpen);
       };
 
-
   return (
-    <ul>
-      {items.map(item => (
-        <AccordionItem key={item.id} item={item} issOpen={openItemId === item.id} 
-        onToggle={() => handleToggle(item.id)}  />
+    <ul className='accordion__list'>
+      {items.map((item,index) => (
+        <AccordionItem key={item.id} item={item} index={index} isOpen={openItemId === item.id} 
+        onToggle={() => handleToggle(item.id)}
+          />
       ))}
     </ul>
   );
