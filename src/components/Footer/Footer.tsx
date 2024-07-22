8fb8826d1e5274382701a313cbac1276ca50b7d1
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-07-22 08:27:28 +0200

    fix mobile responsive getInTouch section, footer, bookingForm form

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,66 +1,87 @@
+import { useMediaQuery } from "react-responsive";
 import "../../scss/layout/_footer.scss";
 import Icon from "../Icon/Icon";
 
 const Footer = () => {
+  const isMobileSm = useMediaQuery({ maxWidth: 1023.99 });
+
   return (
     <footer className="footer">
       <div className="container footer__container">
-        <div className="footer__border"></div>
-        <ul className="footer__list">
-          <li className="footer__list--item">
-            <h3 className="title">About us</h3>
-            <p className="description">
-              The five-star hotel in a beautiful European city with a modern
-              restaurant, conference-hall, and art-bar.
-            </p>
-          </li>
-          <li className="footer__list--item">
-            <h3 className="title">News</h3>
-            <p className="description">
-              Sign up to our newsletter not to miss exclusive offers and
-              information about the upcoming events.
-            </p>
-          </li>
-          <li className="footer__list--item">
-            <h3 className="title">Social</h3>
-            <ul className="social--list">
-              <li>
-                <a target="_blank" rel="noreferrer noopener nofollow" href="#">
-                  Facebook
-                </a>
+        {!isMobileSm ? (
+          <>
+            <ul className="footer__list">
+              <li className="footer__list--item">
+                <h3 className="title">About us</h3>
+                <p className="description">
+                  The five-star hotel in a beautiful European city with a modern
+                  restaurant, conference-hall, and art-bar.
+                </p>
               </li>
-              <li>
-                <a target="_blank" rel="noreferrer noopener nofollow" href="#">
-                  Instagram
-                </a>
+              <li className="footer__list--item">
+                <h3 className="title">News</h3>
+                <p className="description">
+                  Sign up to our newsletter not to miss exclusive offers and
+                  information about the upcoming events.
+                </p>
               </li>
-              <li>
-                <a target="_blank" rel="noreferrer noopener nofollow" href="#">
-                  Twitter
-                </a>
+              <li className="footer__list--item">
+                <h3 className="title">Social</h3>
+                <ul className="social--list">
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noreferrer noopener nofollow"
+                      href="#"
+                    >
+                      Facebook
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noreferrer noopener nofollow"
+                      href="#"
+                    >
+                      Instagram
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noreferrer noopener nofollow"
+                      href="#"
+                    >
+                      Twitter
+                    </a>
+                  </li>
+                </ul>
               </li>
             </ul>
-          </li>
-        </ul>
 
-        <div className="wrapper">
-          <p className="rights">&copy; 2021 All rights reserved. BankHotel</p>
-
-          <a
-            className="email--link"
-            target="_blank"
-            rel="noreferrer noopener nofollow"
-            href="mailto:mailto@gmail.com"
-          >
-            <p>Email</p>
-            <Icon
-              className="email--link__icon"
-              width={31}
-              height={17}
-              iconId="arrow-right"
-            />
-          </a>
-        </div>
+            <div className="wrapper">
+              <p className="rights">
+                &copy;2021 All rights reserved. BankHotel
+              </p>
+              <a
+                className="email--link"
+                target="_blank"
+                rel="noreferrer noopener nofollow"
+                href="mailto:mailto@gmail.com"
+              >
+                <p>Email</p>
+                <Icon
+                  className="email--link__icon"
+                  width={31}
+                  height={17}
+                  iconId="arrow-right"
+                />
+              </a>
+            </div>
+          </>
+        ) : (
+          <p className="rights">&copy;2021 All rights reserved. BankHotel</p>
+        )}
       </div>
     </footer>
   );
--- src/components/GetInTouch/GetInTouch.tsx
+++ src/components/GetInTouch/GetInTouch.tsx
@@ -1,12 +1,14 @@
 import "../../scss/layout/_getInTouch.scss";
 import mgTouch_1920 from "../../assets/images/getInTouchImages/touch-desktop-1920.webp";
 import Icon from "../Icon/Icon";
-import BookingRoomForm from "../BookingRoomForm/BookingRoomForm";
 import { NavProps } from "../../entities/navprops";
 import { useMediaQuery } from "react-responsive";
+import ButtonPoly from "../ButtonPoly/ButtonPoly";
+import BookingRoomFormDatePicker from "../BookingRoomForm/BookingRoomFormDatePicker";
 
 const GetInTouch: React.FC<NavProps> = ({ id }) => {
   const isDesktopLg = useMediaQuery({ minWidth: 1919.98 });
+  const isMobileSm = useMediaQuery({ maxWidth: 1023.99 });
 
   return (
     <section className="section" id={id}>
@@ -15,14 +17,16 @@ const GetInTouch: React.FC<NavProps> = ({ id }) => {
           +38 032 297 50 20
         </a>
 
-        <div className="touch__container--icon-wave--wrapper">
-          {/* <Icon className="icon-wave" iconId="line" /> */}
-          {isDesktopLg ? (
-            <Icon className="icon-wave" iconId="wave-Lg" />
-          ) : (
-            <Icon className="icon-wave" iconId="wave-maxLg" />
-          )}
-        </div>
+        {!isMobileSm && (
+          <div className="touch__container--icon-wave--wrapper">
+            {isDesktopLg ? (
+              <Icon className="icon-wave" iconId="wave-Lg" />
+            ) : (
+              <Icon className="icon-wave" iconId="wave-maxLg" />
+            )}
+          </div>
+        )}
+
         <div className="touch__wrapper">
           <h2 className="touch__wrapper--title">
             Get in
@@ -30,36 +34,38 @@ const GetInTouch: React.FC<NavProps> = ({ id }) => {
             <span className="second-title-part">touch</span>
           </h2>
 
-          <div className="info--wrapper">
-            {/* <Icon
-              className="icon--star"
-              width={150}
-              height={150}
-              iconId="star-16"
-            /> */}
-            <div>
-              <div className="address--info">
-                <a
-                  // className="address--info"
-                  rel="noopener noreferrer nofollow"
-                  target="_blank"
-                  href="https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%9B%D0%B8%D1%81%D1%82%D0%BE%D0%BF%D0%B0%D0%B4%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE+%D0%A7%D0%B8%D0%BD%D0%B0,+8,+%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2,+%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+79000/@49.8409133,24.0207693,19.12z/data=!4m6!3m5!1s0x473add71698ec493:0x7100c9b2c7b810d4!8m2!3d49.8409357!4d24.0209772!16s%2Fg%2F11bw4hghr9?authuser=0&entry=ttu"
-                >
-                  8 Lystopadovoho Chynu, Lviv
-                </a>
-              </div>
-              {/* <div className="wrapper__star--img"> */}
-              <div className="touch__img--wrapper">
-                <img className="touch__img" src={mgTouch_1920} />
-              </div>
-              {/* </div> */}
+          {isMobileSm && (
+            <ButtonPoly
+              className="btnPoly booking intouch"
+              iconWidth={120}
+              iconHeight={120}
+              iconPolygonId="polygon-fill"
+            >
+              <span>Book room</span>
+            </ButtonPoly>
+          )}
+
+          <div className="touch__address--img--wrapper">
+            <div className="address--wrapper">
+              <a
+                rel="noopener noreferrer nofollow"
+                target="_blank"
+                href="https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%9B%D0%B8%D1%81%D1%82%D0%BE%D0%BF%D0%B0%D0%B4%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE+%D0%A7%D0%B8%D0%BD%D0%B0,+8,+%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2,+%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+79000/@49.8409133,24.0207693,19.12z/data=!4m6!3m5!1s0x473add71698ec493:0x7100c9b2c7b810d4!8m2!3d49.8409357!4d24.0209772!16s%2Fg%2F11bw4hghr9?authuser=0&entry=ttu"
+              >
+                8 Lystopadovoho Chynu, Lviv
+              </a>
+            </div>
+            <div className="touch__img--wrapper">
+              <img className="touch__img" src={mgTouch_1920} />
             </div>
           </div>
         </div>
-        <div className="touch__form--wrapper">
-          <h3 className="touch__form--wrapper--title">Find a room</h3>
-          <BookingRoomForm className="touch" />
-        </div>
+        {!isMobileSm && (
+          <div className="touch__form--wrapper">
+            <h3 className="touch__form--wrapper--title">Find a room</h3>
+            <BookingRoomFormDatePicker className="touch" />
+          </div>
+        )}
       </div>
     </section>
   );
