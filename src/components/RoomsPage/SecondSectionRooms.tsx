82650b87c9209778148ba0b3fa9eb3449f337816
StDen <stepaniuk.den@gmail.com>
2024-07-29 11:02:46 +0300

    add responsive second-section

--- src/components/RoomsPage/SecondSectionRooms.tsx
+++ src/components/RoomsPage/SecondSectionRooms.tsx
@@ -1,11 +1,16 @@
+import { useMediaQuery } from "react-responsive";
 import Icon from "../Icon/Icon";
 import ResponsiveImage from "../ResponsiveImg/ResponsiveImg";
 
 const SecondSectionRooms = () => {
+  const isMobile = useMediaQuery({ maxWidth: 1023.98 });
   return (
     <section className="second-rooms">
       <div className="second-rooms__wave-wrapper">
-        <Icon iconId="wave-room-page-1" className="icon-wave" />
+        <Icon
+          iconId={!isMobile ? "wave-room-page-1" : "wave-room-page-2"}
+          className="icon-wave"
+        />
       </div>
       <div className="second-rooms__room">
         <p className="title">Premier Standard</p>
