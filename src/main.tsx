5505b80b54862ea28bd5c77852d68aec61274d38
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2024-07-31 09:18:36 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store, { persistor } from "./redux/store.ts";
-import { PersistGate } from "redux-persist/integration/react";
+import store from "./redux/store.ts";
+// import store, { persistor } from "./redux/store.ts";
+// import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
