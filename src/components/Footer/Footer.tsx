adc9e23ad261e403a758b18b1862c274de49a7af
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-08-05 21:10:35 +0200

    add styles for GetInTouch, Footer on the RestaurantPage, fix header

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,93 +1,98 @@
 import { useMediaQuery } from "react-responsive";
 import "../../scss/layout/_footer.scss";
 import Icon from "../Icon/Icon";
+import { NavProps } from "../../entities/navprops";
 
-const Footer = () => {
+const Footer: React.FC<NavProps> = ({ isRestaurantPage }) => {
   const isMobileSm = useMediaQuery({ maxWidth: 767.98 });
 
   return (
-    <footer className="footer">
-      <div className="container footer__container">
-        {!isMobileSm ? (
-          <>
-            <ul className="footer__list">
-              <li className="footer__list--item">
-                <h3 className="title">
-                  <span className="title__prefix">/</span>About us
-                </h3>
-                <p className="description">
-                  The five-star hotel in a beautiful European city with a modern
-                  restaurant, conference-hall, and art-bar.
-                </p>
-              </li>
-              <li className="footer__list--item">
-                <h3 className="title">
-                  <span className="title__prefix">/</span>News
-                </h3>
-                <p className="description">
-                  Sign up to our newsletter not to miss exclusive offers and
-                  information about the upcoming events.
-                </p>
-              </li>
-              <li className="footer__list--item">
-                <h3 className="title">
-                  <span className="title__prefix">/</span>Socials
-                </h3>
-                <ul className="social--list">
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="noreferrer noopener nofollow"
-                      href="#"
-                    >
-                      Facebook
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="noreferrer noopener nofollow"
-                      href="#"
-                    >
-                      Instagram
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="noreferrer noopener nofollow"
-                      href="#"
-                    >
-                      Twitter
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+    <footer
+      className={`footer ${isRestaurantPage ? "restaurantPageStyles" : ""} `}
+    >
+      <div className="container ">
+        <div className="footer__container">
+          {!isMobileSm ? (
+            <>
+              <ul className="footer__list">
+                <li className="footer__list--item">
+                  <h3 className="title">
+                    <span className="title__prefix">/</span>About us
+                  </h3>
+                  <p className="description">
+                    The five-star hotel in a beautiful European city with a
+                    modern restaurant, conference-hall, and art-bar.
+                  </p>
+                </li>
+                <li className="footer__list--item">
+                  <h3 className="title">
+                    <span className="title__prefix">/</span>News
+                  </h3>
+                  <p className="description">
+                    Sign up to our newsletter not to miss exclusive offers and
+                    information about the upcoming events.
+                  </p>
+                </li>
+                <li className="footer__list--item">
+                  <h3 className="title">
+                    <span className="title__prefix">/</span>Socials
+                  </h3>
+                  <ul className="social--list">
+                    <li>
+                      <a
+                        target="_blank"
+                        rel="noreferrer noopener nofollow"
+                        href="#"
+                      >
+                        Facebook
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        target="_blank"
+                        rel="noreferrer noopener nofollow"
+                        href="#"
+                      >
+                        Instagram
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        target="_blank"
+                        rel="noreferrer noopener nofollow"
+                        href="#"
+                      >
+                        Twitter
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
 
-            <div className="wrapper">
-              <p className="rights">
-                &copy;2021 All rights reserved. BankHotel
-              </p>
-              <a
-                className="email--link"
-                target="_blank"
-                rel="noreferrer noopener nofollow"
-                href="mailto:mailto@gmail.com"
-              >
-                <p>Email</p>
-                <Icon
-                  className="email--link__icon"
-                  width={31}
-                  height={17}
-                  iconId="arrow-right"
-                />
-              </a>
-            </div>
-          </>
-        ) : (
-          <p className="rights">&copy;2021 All rights reserved. BankHotel</p>
-        )}
+              <div className="wrapper">
+                <p className="rights">
+                  &copy;2021 All rights reserved. BankHotel
+                </p>
+                <a
+                  className="email--link"
+                  target="_blank"
+                  rel="noreferrer noopener nofollow"
+                  href="mailto:mailto@gmail.com"
+                >
+                  <p>Email</p>
+                  <Icon
+                    className="email--link__icon"
+                    width={31}
+                    height={17}
+                    iconId="arrow-right"
+                  />
+                </a>
+              </div>
+            </>
+          ) : (
+            <p className="rights">&copy;2021 All rights reserved. BankHotel</p>
+          )}
+        </div>
       </div>
     </footer>
   );
--- src/components/GetInTouch/GetInTouch.tsx
+++ src/components/GetInTouch/GetInTouch.tsx
@@ -12,7 +12,7 @@ import {
   handleClick,
 } from "../../helpers/animationHandleForm";
 
-const GetInTouch: React.FC<NavProps> = ({ id }) => {
+const GetInTouch: React.FC<NavProps> = ({ id, isRestaurantPage }) => {
   const isDesktopLg = useMediaQuery({ minWidth: 1919.98 });
   const isMobileSm = useMediaQuery({ maxWidth: 767.98 });
 
@@ -21,7 +21,10 @@ const GetInTouch: React.FC<NavProps> = ({ id }) => {
   const [isClosing, setIsClosing] = useState(false);
 
   return (
-    <section className="section" id={id}>
+    <section
+      className={`section ${isRestaurantPage ? "restaurantPageStyles" : ""}`}
+      id={id}
+    >
       <div className="container touch__container">
         <a className="touch__container--phone" href="tel:+380322975020">
           +38 032 297 50 20
@@ -37,12 +40,23 @@ const GetInTouch: React.FC<NavProps> = ({ id }) => {
           </div>
         )}
 
+        <div className="address--wrapper">
+          <a
+            rel="noopener noreferrer nofollow"
+            target="_blank"
+            href="https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%9B%D0%B8%D1%81%D1%82%D0%BE%D0%BF%D0%B0%D0%B4%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE+%D0%A7%D0%B8%D0%BD%D0%B0,+8,+%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2,+%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+79000/@49.8409133,24.0207693,19.12z/data=!4m6!3m5!1s0x473add71698ec493:0x7100c9b2c7b810d4!8m2!3d49.8409357!4d24.0209772!16s%2Fg%2F11bw4hghr9?authuser=0&entry=ttu"
+          >
+            8 Lystopadovoho Chynu, Lviv
+          </a>
+        </div>
+
         <div className="touch__wrapper">
           <h2 className="touch__wrapper--title">
             Get in
             <Icon className="icon--star" iconId="star-16" />
             <span className="second-title-part">touch</span>
           </h2>
+
           {isMobileSm && (
             <>
               <ButtonPoly
@@ -69,21 +83,15 @@ const GetInTouch: React.FC<NavProps> = ({ id }) => {
                       setIsClosing
                     )
                   }
-                  touchClassName={styles.mobile}
+                  touchClassName={`${styles.mobile} ${
+                    isRestaurantPage ? styles.restaurantPageStylesForm : ""
+                  }`}
                 />
               )}
             </>
           )}
+
           <div className="touch__address--img--wrapper">
-            <div className="address--wrapper">
-              <a
-                rel="noopener noreferrer nofollow"
-                target="_blank"
-                href="https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%9B%D0%B8%D1%81%D1%82%D0%BE%D0%BF%D0%B0%D0%B4%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE+%D0%A7%D0%B8%D0%BD%D0%B0,+8,+%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2,+%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+79000/@49.8409133,24.0207693,19.12z/data=!4m6!3m5!1s0x473add71698ec493:0x7100c9b2c7b810d4!8m2!3d49.8409357!4d24.0209772!16s%2Fg%2F11bw4hghr9?authuser=0&entry=ttu"
-              >
-                8 Lystopadovoho Chynu, Lviv
-              </a>
-            </div>
             <div className="touch__img--wrapper">
               <ResponsiveImage
                 alt="Room interior"
@@ -93,12 +101,17 @@ const GetInTouch: React.FC<NavProps> = ({ id }) => {
             </div>
           </div>
         </div>
+
         {!isMobileSm && (
           <div className="touch__form--wrapper">
             <h3 className="touch__form--wrapper--title">Find a room</h3>
             <BookingRoomFormDatePicker
-              className="touch"
-              touchClassName={styles.touch}
+              className={`touch ${
+                isRestaurantPage ? "restaurantPageStylesForm" : ""
+              }`}
+              touchClassName={`${styles.touch} ${
+                isRestaurantPage ? styles.restaurantPageStylesForm : ""
+              }`}
             />
           </div>
         )}
